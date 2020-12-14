f94310c1c31777056cc52a888d5233c4e9547fef
d0ge3 <fnik340@gmail.com>
2020-12-14 18:42:26 +0300

    рефакторинг

--- index.js
+++ index.js
@@ -44,31 +44,31 @@ const step = () => {
   isChanged = false
   for (let i = 0; i < M; i++) {
     for (let j = 0; j < N; j++) {
-      let countOfLiving = 0
+      let countLiving = 0
 
       // Проверка верхней тройки соседей
-      i > 0 && desk[i-1][j] === 1 && countOfLiving++
-      i > 0 && j > 0 && desk[i-1][j-1] === 1 && countOfLiving++
-      i > 0 && j < N-1 && desk[i-1][j+1] === 1 && countOfLiving++
+      i > 0 && desk[i-1][j] === 1 && countLiving++
+      i > 0 && j > 0 && desk[i-1][j-1] === 1 && countLiving++
+      i > 0 && j < N-1 && desk[i-1][j+1] === 1 && countLiving++
       
       // Проверка нижней тройки соседей
-      i < M-1 && desk[i+1][j] === 1 && countOfLiving++
-      i < M-1 && j > 0 && desk[i+1][j-1] === 1 && countOfLiving++
-      i < M-1 && j < N-1 && desk[i+1][j+1] === 1 && countOfLiving++
+      i < M-1 && desk[i+1][j] === 1 && countLiving++
+      i < M-1 && j > 0 && desk[i+1][j-1] === 1 && countLiving++
+      i < M-1 && j < N-1 && desk[i+1][j+1] === 1 && countLiving++
       
       // Проверка соседей справа и слева
-      j > 0 && desk[i][j-1] === 1 && countOfLiving++
-      j < N-1 && desk[i][j+1] === 1 && countOfLiving++
+      j > 0 && desk[i][j-1] === 1 && countLiving++
+      j < N-1 && desk[i][j+1] === 1 && countLiving++
 
-      if (desk[i][j] === 1 && countOfLiving < 2) {
+      if (desk[i][j] === 1 && countLiving < 2) {
         desk[i][j] = 0
         isChanged = true
       }
-      if (desk[i][j] === 0 && countOfLiving === 3) {
+      if (desk[i][j] === 0 && countLiving === 3) {
         desk[i][j] = 1
         isChanged = true
       }
-      if (desk[i][j] === 1 && countOfLiving > 3) {
+      if (desk[i][j] === 1 && countLiving > 3) {
         desk[i][j] = 0
         isChanged = true
       }
